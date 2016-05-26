ebdd8c31d7de8619eed4e89bf79f9d42c99e0f9f
moondogg71 <richardstiehm@gmail.com>
2016-05-26 11:57:54 +0000

    added second factorialize

--- basic/factor.js
+++ basic/factor.js
@@ -2,7 +2,19 @@ function factorialize(num) {
     if(num <= 1) {
         return 1;
     } else {
+        console.log("Num: " + num + " Num-1 " + num -1);
         return num * factorialize(num-1)
     }
 }
 factorialize(5);
+
+
+function factorialize2(num){
+  var result = num;
+  while (num > 1) {
+    num--;
+    result = result * num;
+  }
+  return result;
+}
+	
